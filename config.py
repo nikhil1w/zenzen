ef40a43c59859bc11aec00301160d7c6368f8fca
bhaskar16042000 <cap16042000@gmail.com>
2025-08-12 13:31:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,22 +8,22 @@ API_ID = int(os.environ.get("API_ID", "27567486"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH","b1760d4b5ef697bb8da4e7ac4e261c49")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8054981115:AAEexuU-cWgy-6wwzLuz8xTGP19tVkfrYEg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8201116419:AAEVTqxToxQQn_I9TWtJK7-vA9jUFQbpsGg")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@aaaaanimaltxtbot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Cologinid_bot")
 BOT_TEXT = "newtxtbot"
 # ------------------------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "7780806801"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002770753690"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002700941177"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002471859014")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002700941177")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://editingtution99:kLKimOFEX1MN1v0G@cluster0.fxbujjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002770077524"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002700941177"))
 # -----------------------------------------------
 join = '<a href="https://t.me/Alphasto_09_bot">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
@@ -31,7 +31,7 @@ UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "newtxtbot" #without @
+ADMIN_BOT_USERNAME = "Cologinid_bot" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://img.freepik.com/premium-photo/black-car-is-driving-down-road_951586-147043.jpg")
 
